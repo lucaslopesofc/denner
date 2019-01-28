556abc27b2a545d44b9bc1dbb10c41f7a7986e78
Lucas Lopes <lucas@semasi.pmci.es>
2019-01-28 17:19:25 -0200

    Implementação de Login

---
+++ resources/views/vendor/adminlte/login.blade.php
@@ -0,0 +1,78 @@
+@extends('adminlte::master')
+
+@section('adminlte_css')
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/plugins/iCheck/square/blue.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/css/auth.css') }}">
+    @yield('css')
+@stop
+
+@section('body_class', 'login-page')
+
+@section('body')
+    <div class="login-box">
+        <div class="login-box-body">
+        <img class="logo-login" src="http://10.1.19.220/denner/images/logo3.png" alt="Denner Grillo">
+            <p class="login-box-msg">Digite suas credenciais para ter acesso a área.</p>
+            <form action="{{ url(config('adminlte.login_url', 'login')) }}" method="post">
+                {!! csrf_field() !!}
+
+                <div class="form-group has-feedback {{ $errors->has('login') ? 'has-error' : '' }}">
+                    <input type="text" name="login" class="form-control" value="{{ old('login') }}"
+                           placeholder="{{ trans('adminlte::adminlte.login') }}">
+                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
+                    @if ($errors->has('login'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('login') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group has-feedback {{ $errors->has('password') ? 'has-error' : '' }}">
+                    <input type="password" name="password" class="form-control"
+                           placeholder="{{ trans('adminlte::adminlte.password') }}">
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="checkbox icheck">
+                            <label>
+                                <input type="checkbox" name="remember"> {{ trans('adminlte::adminlte.remember_me') }}
+                            </label>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-xs-4">
+                        <button type="submit"
+                                class="btn btn-primary btn-block btn-flat">{{ trans('adminlte::adminlte.sign_in') }}</button>
+                    </div>
+                    <!-- /.col -->
+                </div>
+            </form>
+            <div class="auth-links">
+                <a href="{{ url(config('adminlte.password_reset_url', 'password/reset')) }}"
+                   class="text-center"
+                >{{ trans('adminlte::adminlte.i_forgot_my_password') }}</a>
+                <br>
+            </div>
+        </div>
+        <!-- /.login-box-body -->
+    </div><!-- /.login-box -->
+@stop
+
+@section('adminlte_js')
+    <script src="{{ asset('vendor/adminlte/plugins/iCheck/icheck.min.js') }}"></script>
+    <script>
+        $(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' // optional
+            });
+        });
+    </script>
+    @yield('js')
+@stop
