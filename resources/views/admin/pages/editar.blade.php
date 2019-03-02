ad5294e2b26a541f4c8aea02f0c6c064eedb272e
lucaslopesofc <llopesofc@gmail.com>
2019-03-02 11:20:15 -0300

    Ajeitado Views de Paginas

--- resources/views/admin/pages/editar.blade.php
+++ resources/views/admin/pages/editar.blade.php
@@ -32,82 +32,83 @@
     }
 </style>
 
-<section class="content-header">
-    <div class="row">
-        <div class="col-md-6" style="float: none;margin: 0 auto;">
-            <div class="box box-primary">
-                <div class="box-header with-border">
-                    <h3 class="box-title">Editar Página</h3>
-                </div>
-                <!-- /.box-header -->
-
-                <form action="{{ route('admin.config.pages.update', $pages->id) }}" method="POST" enctype="multipart/form-data" role="form">
-                    {{ csrf_field() }}
-                    <div class="box-body">
-                        <input type="hidden" name="tags" value="{{ $pages->tags }}">
-                        
-                        <div class="form-group">
-                            <label>Título <b class="text-red">*</b></label>
-                            <input type="text" name="title" class="form-control" value="{{ $pages->title }}" maxlength="60" required>
-                            @if ($errors->has('title'))
-                                <span class="text-red">
-                                    <strong>{{ $errors->first('title') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-
-                        <div class="form-group">
-                            <label>Subtítulo</label>
-                            <input type="text" name="subtitle" class="form-control" value="{{ $pages->subtitle }}" maxlength="60">
-                            @if ($errors->has('subtitle'))
-                                <span class="text-red">
-                                    <strong>{{ $errors->first('subtitle') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-
-                        <div class="form-group">
-                            <label>Descrição <b class="text-red">*</b></label>
-                            <textarea name="text" class="form-control" rows="5" value="{{ $pages->text }}" maxlength="600">{{ $pages->text }}</textarea>
-                            @if ($errors->has('text'))
-                                <span class="text-red">
-                                    <strong>{{ $errors->first('text') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-5">
-                                <img id="blah" src="/storage/{{ $pages->image }}" style="width: 200px;height: auto;border-radius: 4px;box-shadow: 0 1px 3px rgba(0,0,0,.15);" />
-                            </div>
-                            <div class="col-md-7">
-                                <div class="fileUpload btn btn-primary">
-                                    <span>Selecione a Imagem</span>
-                                    <input type="file" name="image" value="{{ $pages->image }}" class="upload" onchange="readURL(this);" />
-                                </div>
-                                <p class="help-block">A imagem não deve ser maior que 2MB.</p>
-                                <p class="help-block">Envie apenas formatos de imagem PNG/JPG/JPEG.</p>
-                                @if ($errors->has('image'))
-                                    <span class="text-red">
-                                        <strong>{{ $errors->first('image') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                    </div>
-                    <!-- /.box-body -->
-                    <div class="box-footer">
-                        <button type="submit" class="btn btn-primary pull-right">Enviar</button>
-                        <a href="{{ URL::previous() }}" class="btn btn-danger">Cancelar</a>
-                    </div>
-                </form>
-                <!-- /.box-body -->
+<div class="row">
+    <div class="col-md-6" style="float: none;margin: 0 auto;">
+        <div class="box box-primary">
+            <div class="box-header with-border">
+                <h3 class="box-title">Editar Página</h3>
             </div>
-            <!-- /.box -->
+            <!-- /.box-header -->
+
+            <form action="{{ route('admin.config.pages.update', $pages->id) }}" method="POST" enctype="multipart/form-data" role="form">
+                {{ csrf_field() }}
+                <div class="box-body">
+                    <input type="hidden" name="tags" value="{{ $pages->tags }}">
+                    
+                    <div class="form-group">
+                        <label>Título <b class="text-red">*</b></label>
+                        <input type="text" name="title" class="form-control" value="{{ $pages->title }}" maxlength="60" required>
+                        @if ($errors->has('title'))
+                            <span class="text-red">
+                                <strong>{{ $errors->first('title') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label>Subtítulo</label>
+                        <input type="text" name="subtitle" class="form-control" value="{{ $pages->subtitle }}" maxlength="60">
+                        @if ($errors->has('subtitle'))
+                            <span class="text-red">
+                                <strong>{{ $errors->first('subtitle') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label>Descrição <b class="text-red">*</b></label>
+                        <textarea name="text" class="form-control" rows="5" value="{{ $pages->text }}" maxlength="600">{{ $pages->text }}</textarea>
+                        @if ($errors->has('text'))
+                            <span class="text-red">
+                                <strong>{{ $errors->first('text') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-5">
+                            <img id="blah" src="/storage/{{ $pages->image }}" style="width: 200px;height: auto;border-radius: 4px;box-shadow: 0 1px 3px rgba(0,0,0,.15);" />
+                        </div>
+                        <div class="col-md-7">
+                            <div class="fileUpload btn btn-primary">
+                                <span>Selecione a Imagem</span>
+                                <input type="file" name="image" value="{{ $pages->image }}" class="upload" onchange="readURL(this);" />
+                            </div>
+                            <p></p>
+                            <p class="help-block">A imagem não deve ser maior que 2MB.</p>
+                            <p class="help-block">Envie apenas formatos de imagem PNG/JPG/JPEG.</p>
+                            @if ($errors->has('image'))
+                                <span class="text-red">
+                                    <strong>{{ $errors->first('image') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.box-body -->
+
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-primary pull-right">Enviar</button>
+                    <a href="{{ route('admin.config.pages') }}" class="btn btn-danger">Cancelar</a>
+                </div>
+                <!-- /.box-footer -->
+
+            </form>
         </div>
+        <!-- /.box -->
     </div>
-</section>
+</div>
 
 <script>
     function readURL(input) {
